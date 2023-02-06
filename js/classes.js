b68280c1dba1552aa239a5bf6520712fff876c31
Leonardo Gama <leogama09@gmail.com>
2023-02-06 17:21:03 -0300

    Attack

--- js/classes.js
+++ js/classes.js
@@ -108,6 +108,7 @@ class Fighter extends Sprite {
     }
 
     attack() {
+        this.switchSprite('attack1')
         this.isAttacking = true
         setTimeout(() => {
             this.isAttacking = false
@@ -115,7 +116,14 @@ class Fighter extends Sprite {
     }
 
     switchSprite(sprite) {
+        if (
+            this.image === this.sprites.attack1.image && 
+            this.framesCurrent < this.sprites.attack1.framesMax -1
+        ) 
+            return
+
         switch (sprite) {
+
             case 'idle':
                 if (this.image !== this.sprites.idle.image) {
                     this.image = this.sprites.idle.image
@@ -123,6 +131,7 @@ class Fighter extends Sprite {
                     this.framesCurrent = 0
                 }
             break
+
             case 'run':
                 if (this.image !== this.sprites.run.image) {
                     this.image = this.sprites.run.image
@@ -130,6 +139,7 @@ class Fighter extends Sprite {
                     this.framesCurrent = 0
                 }
             break
+
             case 'jump':
                 if (this.image !== this.sprites.jump.image) {
                     this.image = this.sprites.jump.image
@@ -137,10 +147,19 @@ class Fighter extends Sprite {
                     this.framesCurrent = 0
                 }
             break
+
             case 'fall':
                 if (this.image !== this.sprites.fall.image) {
                     this.image = this.sprites.fall.image
                     this.framesMax = this.sprites.fall.framesMax
+                    this.framesCurrent = 0
+                }
+            break
+
+            case 'attack1':
+                if (this.image !== this.sprites.attack1.image) {
+                    this.image = this.sprites.attack1.image
+                    this.framesMax = this.sprites.attack1.framesMax
                     this.framesCurrent = 0
                 }
             break
