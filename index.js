c5e727c9d3c4917c817d384ae953b8e5544255f1
Leonardo Gama <leogama09@gmail.com>
2023-02-07 11:18:48 -0300

    Receive hit animation

--- index.js
+++ index.js
@@ -66,6 +66,10 @@ const player = new Fighter({
         attack1: {
             imageSrc: './img/samuraiMack/Attack1.png',
             framesMax: 6
+        },
+        takeHit: {
+            imageSrc: './img/samuraiMack/Take Hit - white silhouette.png',
+            framesMax: 4
         }
     },
     attackBox: {
@@ -119,6 +123,9 @@ const enemy = new Fighter({
         attack1: {
             imageSrc: './img/kenji/Attack1.png',
             framesMax: 4
+        }, takeHit: {
+            imageSrc: './img/kenji/Take hit.png',
+            framesMax: 3
         }
     },
     attackBox: {
@@ -200,7 +207,7 @@ function animate() {
         enemy.switchSprite('fall')
     }
 
-    // detect for collision
+    // detect for collision & enemy gets hit
     if(
        rectangularCollision({
         rectangle1: player,
@@ -209,8 +216,8 @@ function animate() {
        player.isAttacking && 
        player.framesCurrent === 4
     ) {
+       enemy.takeHit()
        player.isAttacking = false
-       enemy.health -= 20
        document.querySelector('#enemyHealth').style.width = enemy.health + '%'
     }
 
@@ -219,6 +226,7 @@ function animate() {
         player.isAttacking = false
     }
 
+    // this is where our player gets hit
     if(
         rectangularCollision({
          rectangle1: enemy,
@@ -227,8 +235,8 @@ function animate() {
         enemy.isAttacking && 
         enemy.framesCurrent === 2
      ) {
+        player.takeHit()
         enemy.isAttacking = false
-        player.health -= 20
         document.querySelector('#playerHealth').style.width = player.health + '%'
      }
 
